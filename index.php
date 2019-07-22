b0aceb57dd8c028b589bfd8b91d4e65e73fae06b
Prashant kumar <45692013+Prashant317@users.noreply.github.com>
2019-07-22 20:36:06 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,68 +1,145 @@
 <!DOCTYPE>
-<?php
-include('functions/functions.php');
+<?php 
+session_start();
+include("functions/functions.php");
+
 ?>
 <html>
-<head>
-<title>My Online Shope</title>
-<link rel="stylesheet" href="styles/style.css" media="all" />
-</head>
+	<head>
+		<title>My Online Shop</title>
+		
+		
+	<link rel="stylesheet" href="styles/style.css" media="all" /> 
+	</head>
+	
 <body>
-    <div class="main_wrapper">
-        <div class="header_wrapper">
-            <img id="logo" src="images/logo.gif" />
-            <img id="banner" src="images/ad_banner.gif" />
-        </div>        
-        <div class="menubar">
-            <ul id="menu">
-                <li><a href="">Home</a></li>
-                <li><a href="">All products</a></li>
-                <li><a href="">My account</a></li>
-                <li><a href="">Sign up</a></li>
-                <li><a href="">Shopping cart</a></li>
-                <li><a href="">Contact us</a></li>                
-            </ul>
-            <div id="form">
-                <form method="get" action="results.php" enctype="multipart/form-data">
-                    <input type="text" name="user_query" placeholder="Search a product" />
-                    <input type="submit" name="search" value="search" />
-                </form>
-            </div>
-        </div>
-        <div class="content_wrapper">
-        <div id="sidebar">
-            <div id="sidebar_title">Categories</div>
-            <ul id="cats">
-                <?php
-                    getCategories(); 
-                ?>
-            </ul>
-            <div id="sidebar_title">Brands</div>
-            <ul id="cats">
-                <?php
-                    getBrands(); 
-                ?>
-            </ul>
-        </div>
-        <div id="content_area">
-            <div id="shopping_cart">
-                <span>Welcome Guest!
-                <b style="color:yellow">Shopping cart</b> Total Items:0 Total Price:0 $
-                <a href="cart.php">Go to cart</a>
-                </span>
+	
+	<!--Main Container starts here-->
+	<div class="main_wrapper">
+	
+		<!--Header starts here-->
+		<div class="header_wrapper">
+		
+			<a href="index.php"><img id="logo" src="images/logo.gif" /> </a>
+			<img id="banner" src="images/ad_banner.gif" />
+		</div>
+		<!--Header ends here-->
+		
+		<!--Navigation Bar starts-->
+		<div class="menubar">
+			
+			<ul id="menu">
+				<li><a href="index.php">Home</a></li>
+				<li><a href="all_products.php">All Products</a></li>
+				<li><a href="customer/my_account.php">My Account</a></li>
+				<li><a href="#">Sign Up</a></li>
+				<li><a href="cart.php">Shopping Cart</a></li>
+				<li><a href="#">Contact Us</a></li>
+			
+			</ul>
+			
+			<div id="form">
+				<form method="get" action="results.php" enctype="multipart/form-data">
+					<input type="text" name="user_query" placeholder="Search a Product"/ > 
+					<input type="submit" name="search" value="Search" />
+				</form>
+			
+			</div>
+			
+		</div>
+		<!--Navigation Bar ends-->
+	
+		<!--Content wrapper starts-->
+		<div class="content_wrapper">
+		
+			<div id="sidebar">
+			
+				<div id="sidebar_title">Categories</div>
+				
+				<ul id="cats">
+				
+				<?php getCats(); ?>
+				
+				<ul>
+					
+				<div id="sidebar_title">Brands</div>
+				
+				<ul id="cats">
+					
+					<?php getBrands(); ?>
+				
+				<ul>
+			
+			
+			</div>
+		
+			<div id="content_area">
+			
+			<?php cart(); ?>
+			
+			<div id="shopping_cart"> 
+					
+					<span style="float:right; font-size:17px; padding:5px; line-height:40px;">
+					
+					<?php 
+					if(isset($_SESSION['customer_email'])){
+					echo "<b>Welcome:</b>" . $_SESSION['customer_email'] . "<b style='color:yellow;'>Your</b>" ;
+					}
+					else {
+					echo "<b>Welcome Guest:</b>";
+					}
+					?>
+					
+					<b style="color:yellow">Shopping Cart -</b> Total Items: <?php total_items();?> Total Price: <?php total_price(); ?> <a href="cart.php" style="color:yellow">Go to Cart</a>
+					
+					
+					<?php 
+					if(!isset($_SESSION['customer_email'])){
+					
+					echo "<a href='checkout.php' style='color:orange;'>Login</a>";
+					
+					}
+					else {
+					echo "<a href='logout.php' style='color:orange;'>Logout</a>";
+					}
+					
+					
+					
+					?>
+					
+					
+					
+					</span>
+			</div>
+			
+				<div id="products_box">
+				
+				<?php getPro(); ?>
+				<?php getCatPro(); ?>
+				<?php getBrandPro(); ?>
+				
+				</div>
+			
+			</div>
+		</div>
+		<!--Content wrapper ends-->
+		
+		
+		
+		<div id="footer">
+		
+		<h2 style="text-align:center; padding-top:30px;">&copy; 2014 by www.OnlineTuting.com</h2>
+		
+		</div>
+	
+	
+	
+	
+	
+	
+	</div> 
+<!--Main Container ends here-->
 
-            </div>
-            <div id="products_box">
-                <?php getProducts();?>
-            </div>
-        </div>
-        </div>
-        <div id="footer">
-            <h2 style="text-align: center; padding-top: 30px;">&copy; 2016 www.onlineTuting.com</h2>;
-        </div>
-    </div>
+
 </body>
-</html?
-<ul id = "container">
-    <li id = "id1" > </li>
-</ul>
+</html>
